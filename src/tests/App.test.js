9fcc34098976d4b3f4bcd7c4e8f24346d18fea31
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-22 15:07:35 -0300

    App-test: Define renderWithRouter e testa rota.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Teste do componente App', () => {
+  const renderWithRouter = (component) => {
+    const history = createMemoryHistory();
+    return ({
+      ...render(<Router history={ history }>{component}</Router>), history,
+    });
+  };
+
+  it('Teste de rota', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toEqual('/');
+  });
+});
+
+
