cc1a697c6e10cfa8bcc7328943931518f6e8741e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-22 16:04:09 -0300

    About-test: testa componentes da page About.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Teste do componente About', () => {
+  const renderWithRouter = (component) => {
+    const history = createMemoryHistory();
+    return ({
+      ...render(<Router history={ history }>{component}</Router>), history,
+    });
+  };
+  it('Teste de rota', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toEqual('/');
+    history.push('/about');
+    expect(history.location.pathname).toEqual('/about');
+  });
+  it('Teste dos componentes da page About', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toEqual('/');
+    history.push('/about');
+    expect(history.location.pathname).toEqual('/about');
+    const title = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(title).toBeInTheDocument();
+    const image = screen.getByAltText(/pokédex/i);
+    expect(image).toBeInTheDocument();
+    const IMAGE_URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    console.log(image);
+    expect(image.src).toEqual(IMAGE_URL);
+    const firstP = screen.getByText(/this application simulates a pokédex/i);
+    expect(firstP).toBeInTheDocument();
+    const seccondP = screen.getByText(/one can filter pokémons by type,/i);
+    expect(seccondP).toBeInTheDocument();
+  });
+});
